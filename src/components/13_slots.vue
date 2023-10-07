1f5e2b1543f14dd98bcfe6f4f831c251c82f0f8f
JiauZhang <1743960454@qq.com>
2023-10-07 19:08:18 +0800

    update

--- src/components/13_slots.vue
+++ src/components/13_slots.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import Emit from "./01_get_started.vue";
+import Slots from "./01_get_started.vue";
 const msg = ref("slot message!");
 </script>
 
 <template>
   <!-- https://vuejs.org/guide/components/slots.html#slot-content-and-outlet -->
   <!-- multiple slot outlets in a single component: https://vuejs.org/guide/components/slots.html#named-slots-->
-  <Emit :prop-one="13" prop-two="Slots">
+  <Slots :prop-one="13" prop-two="Slots">
     <p>{{ msg }}</p>
-  </Emit>
+  </Slots>
 </template>
